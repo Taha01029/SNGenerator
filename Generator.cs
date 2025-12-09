32c81d47b11d7317df234e747fbdb837b73feaa8
Taha <hkmttazhmhmd@gmail.com>
2025-12-09 19:46:22 +0330

    Files Message.cs and Generator.cs Were Added To The Project And Were Also Tested(no issues found for now). The .gitignore File Was Added To The Project And Directories: obj And bin Were Written to the File. Files Messages.txt, Names.txt and Adjectives.txt Were Addeed To The Project And Were Filled With Sample Texts

---
+++ Generator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace SNGenerator
+{
+    public class Generator
+    {
+        readonly private StreamReader _namesReader;
+        readonly private StreamReader _adjReader;
+        readonly private Random random = new Random();
+
+
+        private string GetName()
+        {
+            string[] names = _namesReader.ReadToEnd().Split('$');
+            return names[random.Next(names.Length)];
+        }
+
+        private string GetAdjective()
+        {
+            string[] adjectives = _adjReader.ReadToEnd().Split('$');
+            return adjectives[random.Next(adjectives.Length)];
+        }
+
+        public string GetSN()
+        {
+            return $"{GetName()} {GetAdjective()}";
+        }
+
+        public Generator()
+        {
+            _namesReader = new StreamReader("C:\\Users\\Lenovo\\source\\repos\\SNGenerator\\Names.txt");
+            _adjReader = new StreamReader("C:\\Users\\Lenovo\\source\\repos\\SNGenerator\\Adjectives.txt");
+        }
+
+        ~Generator()
+        {
+            _adjReader.Close();
+            _namesReader.Close();
+        }
+
+        public static Generator GetInstance() => new Generator();
+    }
+}
---
+++ Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace SNGenerator
+{
+    public class Messages
+    {
+        private readonly StreamReader _msgReader;
+        private readonly string[] messages;
+
+        public string this[int index]
+        {
+            get
+            {
+                return messages[index];
+            }
+        }
+
+        public Messages()
+        {
+            _msgReader = new StreamReader("C:\\Users\\Lenovo\\source\\repos\\SNGenerator\\Messages.txt");
+            messages = _msgReader.ReadToEnd().Split('$');
+        }
+
+        ~Messages()
+        {
+            _msgReader.Close();
+        }
+
+        public static Messages GetInstance() => new Messages();
+
+    }
+}
